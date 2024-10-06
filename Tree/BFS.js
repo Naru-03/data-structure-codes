7bf62cb37ed2f7cba662cb8c35d0385d6d30964d
Narayan <choudharynarayan041@gmail.com>
2024-10-06 09:39:33 +0530

    BFS

---
+++ Tree/BFS.js
@@ -0,0 +1,117 @@
+// Breadth first search
+
+//    10
+// 8     12
+//6  9  11   13
+
+
+//  BFS array =[10,8,12,6,9,11,13]
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        const newNode = new Node(value);
+        if (this.root === null) {
+            this.root = newNode;
+            return this;
+        }
+
+        let temp = this.root;
+
+        while (true) {
+
+            if (newNode.value === temp.value) {
+                return undefined;
+            }
+            if (newNode.value < temp.value) {
+                if (temp.left === null) {
+                    temp.left = newNode;
+                    return this;
+                } else {
+                    temp = temp.left;
+                }
+            } else {
+                if (temp.right === null) {
+                    temp.right = newNode;
+                    return this;
+                } else {
+                    temp = temp.right;
+                }
+            }
+        }
+    }
+
+    includes(value) {
+        if (this.root === null || !this.root) {
+            return false;
+        }
+        if (this.root.value === value) {
+            return true;
+        }
+
+        let temp = this.root;
+
+        while (temp) {
+            if (temp.value > value) {
+                temp = temp.left
+            }
+            else if (temp.value < value) {
+                temp = temp.right
+            } else {
+                return true;
+            }
+        }
+
+
+        return false
+    }
+
+    bfs() {
+        let queue = [];
+        let data = [];
+        let temp = this.root;
+
+        queue.push(temp);
+
+        while (queue.length > 0) {
+            temp = queue.shift();
+            data.push(temp.value);
+
+            if (temp.left) {
+                queue.push(temp.left)
+            }
+            if (temp.right) {
+                queue.push(temp.right)
+            }
+        }
+
+        return data;
+    }
+}
+const node = new BinarySearchTree();
+console.log(node);
+
+node.insert(10);
+node.insert(20);
+node.insert(5);
+node.insert(15);
+
+console.log(node);
+
+node.includes(15)
+console.log(node.includes(5))
+// includes method
+
+// bfs method to print breadth first search tree elements 10, 20, 5, 15
+console.log(node.bfs())
